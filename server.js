bb9a4b38d5c9b1bff5a93a57202f84b56ced7baa
DevFahad <github.com/Fahad4345>
2025-09-20 17:47:16 +0500

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.get("/", (req, res) => {
 });
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log("PORT from env:", process.env.PORT);
 });
 process.on("uncaughtException", (err) => {
   console.error("🚨 Uncaught Exception:", err);
