172541a90e52dc5e278af280ba0b136d5c2d27ad
DevFahad <github.com/Fahad4345>
2025-09-23 17:34:54 +0500

    Changes to be committed: 	modified:   server.js

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ connectDB();
 
 const app = express();
 app.use(cors({ origin: "*", credentials: true }));
-
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/stripe", stripeRoute);
 app.use(express.json());
-app.use("/api/auth", authRoutes);
 
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("🚀 Railway API is live!");
 });
