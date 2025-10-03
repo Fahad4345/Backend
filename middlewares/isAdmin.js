bf38955c37f0e30a8a8d51b1c56cfd91dd3c27c9
DevFahad <github.com/Fahad4345>
2025-10-03 20:05:11 +0500

    Changes to be committed: 	modified:   middlewares/isAdmin.js

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,14 +1,27 @@
-export const isAdmin = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token) return res.status(401).json({ error: "No token provided" });
+import jwt from "jsonwebtoken";
 
-  jwt.verify(token, ACCESS_SECRET, (err, decoded) => {
-    if (err) return res.status(403).json({ error: "Invalid token" });
+export const isAdmin = (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    console.log("Token", authHeader);
+    if (!authHeader) {
+      return res.status(401).json({ error: "Authorization header missing" });
+    }
+    console.log("Token", authHeader);
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ error: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
+
     if (decoded.role !== "admin") {
       return res.status(403).json({ error: "Access denied, not admin" });
     }
+
     req.user = decoded;
     next();
-  });
+  } catch (err) {
+    return res.status(403).json({ error: "Invalid or expired token" });
+  }
 };
